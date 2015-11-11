fc1a9a3b4b85474246d9daf23c62df888a240d3e
jarnault <jeremy.arnault@gmail.com>
2015-11-11 15:52:02 +0100

    Pour de vrai maintenant

---
+++ app/src/main/java/com/enseirb/timtim/map_eirb/business/IMapBusiness.java
@@ -0,0 +1,9 @@
+package com.enseirb.timtim.map_eirb.business;
+
+/**
+ * Created by Jeremy on 11/11/2015.
+ */
+public interface IMapBusiness {
+
+
+}
---
+++ app/src/main/java/com/enseirb/timtim/map_eirb/model/POICollection.java
@@ -0,0 +1,9 @@
+package com.enseirb.timtim.map_eirb.model;
+
+import java.util.Collection;
+
+public class POICollection {
+
+    private Collection<POI> poiCollection;
+
+}
