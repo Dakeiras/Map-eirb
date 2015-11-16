f39bcc35d0b4a6a2cef7fb2c274fc2cbdd752c8a
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2015-11-16 15:31:26 +0100

    added un truc obscure

--- app/src/main/java/com/enseirb/timtim/map_eirb/converter/v2/POICollectionConverter2.java
+++ app/src/main/java/com/enseirb/timtim/map_eirb/converter/v2/POICollectionConverter2.java
@@ -2,11 +2,7 @@ package com.enseirb.timtim.map_eirb.converter.v2;
 
 import com.enseirb.timtim.map_eirb.converter.IPOICollectionConverter;
 import com.enseirb.timtim.map_eirb.converter.listener.IPOICollectionConverterListener;
-import com.enseirb.timtim.map_eirb.dao.DefibrillatorCollectionDAO;
-import com.enseirb.timtim.map_eirb.dao.ElectricCollectionDAO;
 import com.enseirb.timtim.map_eirb.dao.IPOICollectionDAO;
-import com.enseirb.timtim.map_eirb.dao.InternetCollectionDAO;
-import com.enseirb.timtim.map_eirb.dao.ToiletCollectionDAO;
 import com.enseirb.timtim.map_eirb.dao.listener.IPOICollectionDAOListener;
 import com.enseirb.timtim.map_eirb.dto.POICollectionDTO;
 import com.enseirb.timtim.map_eirb.dto.POIDTO;
@@ -17,34 +13,47 @@ public class POICollectionConverter2 implements IPOICollectionConverter {
     private IPOICollectionDAO dao;
     private IPOIConverter converter;
 
+        /**
+         * Return a new innstance of the wanted class given its type.
+         * @param packagePath The path of the package in which the class is
+         * @param poiType The type of the POI
+         * @param suffix The suffix of the className. Can whether be "CollectionConverter" or "CollectionDAO"
+         * @return an instance of the wanted class
+         */
+        private Object getInstanceFromClassName(String packagePath, POIType poiType, String suffix){
+            String className = poiType.toString().toLowerCase();
+            className = className.substring(0, 1).toUpperCase() + className.substring(1);
+            String classPath = packagePath + "." + className;
+            classPath += suffix;
+            Object retVal = null;
+            try {
+                retVal = Class.forName(classPath).newInstance();
+            } catch (InstantiationException | ClassNotFoundException | IllegalAccessException e) {
+                System.err.println("Class " + classPath + "does not exist");
+                e.printStackTrace();
+                System.exit(1);
+            }
+            return retVal;
+        }
+
+        private IPOICollectionDAO getDaoCollectionFromName(POIType poiType){
+            return (IPOICollectionDAO) getInstanceFromClassName(
+                    "com.enseirb.timtim.map_eirb.dao", poiType, "CollectionDAO");
+        }
+
+        private IPOIConverter getConvertercollectionFromName(POIType poiType){
+            return (IPOIConverter) getInstanceFromClassName(
+                    "com.enseirb.timtim.map_eirb.converter", poiType, "CollectionConverter");
+
+        }
+
+
 
     public POICollectionConverter2(POIType type) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
-        //this.dao = (IPOICollectionDAO) Class.forName(type.toString()+"CollectionDAO").newInstance();
-        //this.converter = (IPOIConverter) Class.forName(type.toString()+"Converter").newInstance();
-        //OR :
-        switch (type){
-            case DEFIBRILLATOR:
-                dao = new DefibrillatorCollectionDAO();
-                converter = new DefibrillatorConverter();
-                break;
-            case INTERNET:
-                dao = new InternetCollectionDAO();
-                converter = new InternetConverter();
-                break;
-            case TOILET:
-                dao = new ToiletCollectionDAO();
-                converter = new ToiletConverter();
-                break;
-            case ELECTRIC:
-                dao = new ElectricCollectionDAO();
-                converter = new ElectricConverter();
-                break;
-            default:
-                dao = null;
-                converter = null;
-        }
+        type = POIType.DEFIBRILLATOR;
+        this.dao = getDaoCollectionFromName(type);
+        this.converter  =getConvertercollectionFromName(type);
     }
-
     public void retrievePOICollection(final POIType type, final IPOICollectionConverterListener listener){
         dao.retrievePOICollection(new IPOICollectionDAOListener() {
 
